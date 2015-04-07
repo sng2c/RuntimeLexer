4279a49b4d4159b264d939563c1d00b4e94a81d5
sng2c <sng2nara@gmail.com>
2015-04-07 18:33:51 +0900

    테스트 예시 추가

--- src/test/java/com/mabook/java/runtimelexer/application/KoreaBankSmsParserTest.java
+++ src/test/java/com/mabook/java/runtimelexer/application/KoreaBankSmsParserTest.java
@@ -29,5 +29,31 @@ public class KoreaBankSmsParserTest {
 		System.out.println("");
 		KoreaBankSmsParser.parse("[Web발신]\n" +
 				"하나(6*1*)김*승님 04/05 21:13 페리카나 일시불/17,000원/누적1,387,626원", listener);
+
+/*
+BANK:국민은행
+MONEY:2000원
+
+HEADER:[Web발신]
+CARD:하나체크카드(2*7*)
+WORD:김*승님
+DATE:04/05
+TIME:12:00
+MONEY:USD16.47
+WORD:해외승인
+WORD:GOOGLE
+WORD:*SQUARE
+WORD:ENIX
+
+HEADER:[Web발신]
+CARD:하나(6*1*)
+WORD:김*승님
+DATE:04/05
+TIME:21:13
+WORD:페리카나
+WORD:일시불
+MONEY:17,000원
+MONEY_TOTAL:누적1,387,626원
+*/
 	}
 }
